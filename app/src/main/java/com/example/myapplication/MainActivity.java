e6a1d91c6a01b19ec619e06c71fb30efd60902d5
叶明林 <942481251@qq.com>
2017-07-27 16:36:46 +0800

    合并完成

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.Window;
@@ -23,7 +36,7 @@ import java.util.TimerTask;
 
 
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private TextView textView_forehand;
     private TextView textView_backhand;
     private TextView textView_time;
@@ -57,6 +70,8 @@ public class MainActivity extends AppCompatActivity{
     public final static int UPDATETIME=0;
     public final static int UPDATEHAND=1;
 
+    private static final int REQUEST = 1;
+
     private TimerActivity myTimer;
     private Handler mainHandler=new Handler() {
         @Override
@@ -75,10 +90,28 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        ActionBar actionBar = getSupportActionBar();
+        /*ActionBar actionBar = getSupportActionBar();
         actionBar.setLogo(R.drawable.pic1);
         actionBar.setDisplayUseLogoEnabled(true);
-        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);*/
+        try
+        {
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.main_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            drawer.setDrawerListener(toggle);
+            toggle.syncState();
+
+            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(this);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
 
         textView_backhand =(TextView)findViewById(R.id.textView_fan);
         textView_forehand =(TextView)findViewById(R.id.textView_zheng);
@@ -149,6 +182,73 @@ public class MainActivity extends AppCompatActivity{
         super.onResume();
         this.mediaPlayerManager.reStartVideo();
     }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.main_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_file) {
+            Intent intent = new Intent();
+            intent.setClass(MainActivity.this, ExDialog.class);
+            startActivityForResult(intent, REQUEST);
+        } else if (id == R.id.nav_training){
+
+        } else if (id == R.id.nav_statistic) {
+
+        }  else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.main_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode,Intent intent) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST) {
+                Uri uri = intent.getData();
+                new AlertDialog.Builder(this).setMessage(" "+ uri).show();
+            }
+        }
+    }
+
     private class TaskForChart extends TimerTask
     {
         private List<Double> dataList=new ArrayList<Double>();
