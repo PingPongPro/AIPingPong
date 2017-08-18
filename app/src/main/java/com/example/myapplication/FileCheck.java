ddc857cf7a3f9a9287cc2da04bfcdada7a584804
叶明林 <942481251@qq.com>
2017-08-18 17:29:25 +0800

    录像保存

---
+++ app/src/main/java/com/example/myapplication/FileCheck.java
@@ -0,0 +1,50 @@
+package com.example.myapplication;
+
+import java.io.File;
+
+/**
+ * Created by SKY on 2017/8/18.
+ */
+
+public class FileCheck {
+    public boolean isValidFileName(String fileName){
+        if (fileName == null || fileName.length() > 255)
+            return false;
+        else
+            return fileName.matches("[^\\s\\\\/:\\*\\?\\\"<>\\|](\\x20|[^\\s\\\\/:\\*\\?\\\"<>\\|])*[^\\s\\\\/:\\*\\?\\\"<>\\|\\.]$");
+    }
+    public void renameFileName(String oldPath, String newPath){
+        File file = new File(oldPath);
+        file.renameTo(new File(newPath));
+    }
+    public boolean deleteFile(String filePath) {
+        File file = new File(filePath);
+        if (file.isFile() && file.exists()) {
+            return file.delete();
+        }
+        return false;
+    }
+    public boolean deleteDirectory(String filePath) {
+        boolean flag = false;
+        if (!filePath.endsWith(File.separator)) {
+            filePath = filePath + File.separator;
+        }
+        File dirFile = new File(filePath);
+        if (!dirFile.exists() || !dirFile.isDirectory()) {
+            return false;
+        }
+        flag = true;
+        File[] files = dirFile.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isFile()) {
+                flag = deleteFile(files[i].getAbsolutePath());
+                if (!flag) break;
+            } else {
+                flag = deleteDirectory(files[i].getAbsolutePath());
+                if (!flag) break;
+            }
+        }
+        if (!flag) return false;
+        return dirFile.delete();
+    }
+}
--- app/src/main/java/com/example/myapplication/PlayBack.java
+++ app/src/main/java/com/example/myapplication/PlayBack.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.hardware.Camera;
 import android.media.AudioManager;
@@ -11,12 +13,14 @@ import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -30,7 +34,7 @@ import java.util.Calendar;
 
 public class PlayBack extends Activity implements SurfaceHolder.Callback {
 
-    private static final String TAG = "MainActivity";
+    private static final String TAG = "PlayBack";
     private SurfaceView mSurfaceview;
     private Button mBtnStartStop;
     private Button mBtnPlay;
@@ -42,16 +46,14 @@ public class PlayBack extends Activity implements SurfaceHolder.Callback {
     private Camera camera;
     private MediaPlayer mediaPlayer;
     private String path;
+    private File dir, folderPath;
     private TextView textView;
     private int text = 0;
+    private String DateNow;
 
-    private Drawable VIDEO_PAUSE;
-    private Drawable VIDEO_START;
+    private Drawable RECORDING;
+    private Drawable RECORDINGSTOP;
 
-    private SeekBar seekBar;
-    private LinearLayout videoPlay_BigScreen;
-    private RelativeLayout wholeLayout;
-    private int CONTROLLERHEIGHT = 90;
     private boolean isShowController = true;
 
     private android.os.Handler handler = new android.os.Handler();
@@ -64,16 +66,6 @@ public class PlayBack extends Activity implements SurfaceHolder.Callback {
         }
     };
 
-    private void startAnimHide(){
-        ObjectAnimator.ofFloat(videoPlay_BigScreen, View.TRANSLATION_Y,
-                0, DensityUtil.dip2px(this, this.CONTROLLERHEIGHT))
-                .setDuration(200).start();
-    }
-    private void startAnimShow(){
-        ObjectAnimator.ofFloat(videoPlay_BigScreen, View.TRANSLATION_Y,
-                DensityUtil.dip2px(this, this.CONTROLLERHEIGHT), 0)
-                .setDuration(200).start();
-    }
 
     private void myListener(){
         mBtnStartStop.setOnClickListener(new View.OnClickListener() {
@@ -124,16 +116,21 @@ public class PlayBack extends Activity implements SurfaceHolder.Callback {
 
                         path = getSDPath();
                         if (path != null) {
-                            File dir = new File(path + "/recordtest");
+                            DateNow = getDate();
+                            dir = new File(path + "/ballGame/Video");
                             if (!dir.exists()) {
                                 dir.mkdir();
                             }
-                            path = dir + "/" + getDate() + ".mp4";
+                            folderPath = new File(dir + "/" + DateNow);
+                            if(!folderPath.exists()){
+                                folderPath.mkdir();
+                            }
+                            path = folderPath + "/" + DateNow + ".mp4";
                             mRecorder.setOutputFile(path);
                             mRecorder.prepare();
                             mRecorder.start();
                             mStartedFlg = true;
-                            mBtnStartStop.setBackground(VIDEO_PAUSE);
+                            mBtnStartStop.setBackground(RECORDINGSTOP);
                         }
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -147,11 +144,37 @@ public class PlayBack extends Activity implements SurfaceHolder.Callback {
                             mRecorder.reset();
                             mRecorder.release();
                             mRecorder = null;
-                            mBtnStartStop.setBackground(VIDEO_START);
+                            mBtnStartStop.setBackground(RECORDING);
                             if (camera != null) {
                                 camera.release();
                                 camera = null;
                             }
+                            final EditText ETmp = new EditText(PlayBack.this);
+                            ETmp.setHint(DateNow);
+                            ETmp.setHintTextColor(Color.GRAY);
+                            new AlertDialog.Builder(PlayBack.this)
+                                    .setTitle("请输入保存文件夹")
+                                    .setView(ETmp)
+                                    .setPositiveButton("保存", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if(ETmp.getText() != null){
+                                                FileCheck FCTmp = new FileCheck();
+                                                if(FCTmp.isValidFileName(ETmp.getText().toString())){
+                                                    String newPath = folderPath + "/" + ETmp.getText().toString();
+                                                    FCTmp.renameFileName(folderPath.getPath(), newPath);
+                                                }
+                                            }
+                                        }
+                                    })
+                                    .setNegativeButton("不保存", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            FileCheck FCTmp = new FileCheck();
+                                            FCTmp.deleteDirectory(folderPath.getPath());
+                                        }
+                                    })
+                                    .show();
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -161,40 +184,28 @@ public class PlayBack extends Activity implements SurfaceHolder.Callback {
             }
         });
 
-        mBtnPlay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mIsPlay = true;
-                mImageView.setVisibility(View.GONE);
-                mBtnPlay.setBackground(VIDEO_PAUSE);
-                if (mediaPlayer == null) {
-                    mediaPlayer = new MediaPlayer();
-                }
-                mediaPlayer.reset();
-                Uri uri = Uri.parse(path);
-                mediaPlayer = MediaPlayer.create(PlayBack.this, uri);
-                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-                mediaPlayer.setDisplay(mSurfaceHolder);
-                try{
-                    mediaPlayer.prepare();
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-                mediaPlayer.start();
-            }
-        });
-        wholeLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(isShowController){
-                    startAnimHide();
-                }
-                else{
-                    startAnimShow();
-                }
-                isShowController = !isShowController;
-            }
-        });
+//        mBtnPlay.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                mIsPlay = true;
+//                mImageView.setVisibility(View.GONE);
+//                mBtnPlay.setBackground(VIDEO_PAUSE);
+//                if (mediaPlayer == null) {
+//                    mediaPlayer = new MediaPlayer();
+//                }
+//                mediaPlayer.reset();
+//                Uri uri = Uri.parse(path);
+//                mediaPlayer = MediaPlayer.create(PlayBack.this, uri);
+//                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+//                mediaPlayer.setDisplay(mSurfaceHolder);
+//                try{
+//                    mediaPlayer.prepare();
+//                }catch (Exception e){
+//                    e.printStackTrace();
+//                }
+//                mediaPlayer.start();
+//            }
+//        });
     }
 
     @Override
@@ -205,14 +216,12 @@ public class PlayBack extends Activity implements SurfaceHolder.Callback {
         mSurfaceview = (SurfaceView) findViewById(R.id.surfaceview);
         mImageView = (ImageView) findViewById(R.id.imageview);
         mBtnStartStop = (Button) findViewById(R.id.btnStartStop);
-        mBtnPlay = (Button) findViewById(R.id.btnPlayVideo);
+        //mBtnPlay = (Button) findViewById(R.id.btnPlayVideo);
         //textView = (TextView)findViewById(R.id.text);
-        seekBar = (SeekBar)findViewById(R.id.seekbar_BigScreen);
-        wholeLayout = (RelativeLayout)findViewById(R.id.wholeLayout);
-        videoPlay_BigScreen = (LinearLayout)findViewById(R.id.videoPlay_BigScreen);
 
-        VIDEO_START = getResources().getDrawable(R.drawable.video_start);
-        VIDEO_PAUSE = getResources().getDrawable(R.drawable.video_pause);
+        RECORDING = getResources().getDrawable(R.drawable.recording);
+        RECORDINGSTOP = getResources().getDrawable(R.drawable.recording_stop);
+
         myListener();
         SurfaceHolder holder = mSurfaceview.getHolder();
         holder.addCallback(this);
@@ -229,6 +238,11 @@ public class PlayBack extends Activity implements SurfaceHolder.Callback {
         if (!mStartedFlg) {
             mImageView.setVisibility(View.VISIBLE);
         }
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
     }
 
     /**
